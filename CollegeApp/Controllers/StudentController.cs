1ff3db529132b00bbd25a6fe85f51db553df6ccb
zaibo2762 <77626531+zaibo2762@users.noreply.github.com>
2024-05-29 17:36:35 +0500

    second commit

--- CollegeApp/Controllers/StudentController.cs
+++ CollegeApp/Controllers/StudentController.cs
@@ -1,5 +1,6 @@
 ﻿using CollegeApp.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CollegeApp.Controllers
@@ -86,6 +87,60 @@ namespace CollegeApp.Controllers
             model.Id = student.Id;
             return Ok(model);
         }
+        [HttpPut]
+        public ActionResult UpdateStudent([FromBody] StudentDTO model)
+        {
+            if(model == null | model.Id <= 0)
+            {
+                return BadRequest();
+            }
+            var existingStudent = CollegeRepository.Students.Where(s=> s.Id == model.Id).FirstOrDefault();
+            if(existingStudent == null) {
+                return NotFound();
+            }
+            
+            existingStudent.Id = model.Id;
+            existingStudent.StudentName = model.StudentName;
+            existingStudent.Email = model.Email;
+            existingStudent.Address = model.Address;
+            return NoContent();
+
+        }
+
+        [HttpPatch]
+        public ActionResult UpdateStudentPartial( int id,[FromBody] JsonPatchDocument<StudentDTO> patchdocument)
+        {
+            if (patchdocument == null | id <= 0)
+            {
+                return BadRequest();
+            }
+            var existingStudent = CollegeRepository.Students.Where(s => s.Id == id).FirstOrDefault();
+            if (existingStudent == null)
+            {
+                return NotFound();
+            }
+            var studentDTO = new StudentDTO
+            {
+                Id = existingStudent.Id,
+                StudentName = existingStudent.StudentName,
+                Email = existingStudent.Email,
+                Address = existingStudent.Address,
+            };
+            patchdocument.ApplyTo(studentDTO, ModelState);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+
+            existingStudent.Id = studentDTO.Id;
+            existingStudent.StudentName = studentDTO.StudentName;
+            existingStudent.Email = studentDTO.Email;
+            existingStudent.Address = studentDTO.Address;
+            return NoContent();
+
+        }
+
         [HttpDelete]
         [Route("{id}", Name = "DeleteStudentByID")]
         public ActionResult<bool> DeleteStudent(int id)
